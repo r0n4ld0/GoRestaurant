9ea0c5288a3f8a8e955953cb1dbed88e43cde3c7
r0n4ld0 <r0n4ld0@gmail.com>
2020-06-26 23:00:19 -0300

    challenge done

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,6 +2,10 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: 'http://localhost:3333',
+  headers: {
+    accept: 'application/json',
+  },
+  data: {},
 });
 
 export default api;
